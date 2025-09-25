24a202794049421838c6eac28bb3ff3a083c358f
ChetanAnandaReddyKukutla <chetananandareddykukutla@gmail.com>
2025-09-26 00:27:11 +0530

    add runtime

--- app/main.py
+++ app/main.py
@@ -5,12 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:3000",
-    "https://productive-boards-frontend.vercel.app/",
+    "https://productive-boards-frontend.vercel.app",  
 ]
 
 app.add_middleware(
@@ -20,6 +16,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 Base.metadata.create_all(bind=engine)
 
